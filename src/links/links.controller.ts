65612310abe7c6d36a7c5516f8ca154cd930c56e
Ifeoluwa Arowosegbe <iifeoluwa.ao@gmail.com>
2021-11-23 00:45:28 +0100

    feat: enable versioning and create swagger docs

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -2,7 +2,10 @@ import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { CreateLinkDto } from './dtos/links.dto';
 import { LinksService } from './links.service';
 
-@Controller('/links')
+@Controller({
+  version: '1',
+  path: '/links'
+})
 export class LinksController {
   constructor(private readonly linksService: LinksService) { }
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -10,6 +11,19 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v'
+  });
+
+  const docsConfig = new DocumentBuilder()
+    .setTitle('URL shortener')
+    .setDescription('The ultimate url shortening experience')
+    .setVersion('1.0')
+    .addTag('links')
+    .build();
+  const document = SwaggerModule.createDocument(app, docsConfig);
+  SwaggerModule.setup('docs', app, document);
 
   const port = configService.get('app.port');
   await app.listen(port);
